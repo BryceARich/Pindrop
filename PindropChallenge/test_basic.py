4b10785a0ea501e812d62a3cd4d441b60bf657d7
Sunappsis <sunappsisllc@gmail.com>
2017-11-15 23:41:26 -0500

    Added pytests for phone numbers and number of comments

--- PindropChallenge/test_basic.py
+++ PindropChallenge/test_basic.py
@@ -60,10 +60,47 @@ class BasicTests(unittest.TestCase):
         for f in fetched:
             area_code = f.get("area_code")
             self.assertEqual(len(area_code),3)
-            match = re.match("(\d{3})", area_code)
-            print area_code
+            match = re.match("(\d{3})$", area_code)
             assert match is not None
             self.assertEquals(len(match.groups()), 1) # ensures 3 digits
+
+    # regex check using a regex equation from https://www.safaribooksonline.com/library/view/regular-expressions-cookbook/9781449327453/ch04s02.html
+    def test_has_full_number(self):
+        response = app.get_results(1)
+        fetched = json.loads(response)
+        phone_number = fetched[0].get("phone_number")
+        match = re.match("\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$",phone_number)
+        assert match is not None
+
+    # regex pattern should also work for phone numbers of ten digits, however it's not currently functional for +1 phone numbers so it currently assumes local country area codes
+    def test_all_full_numbers(self):
+        response = app.get_results()
+        fetched = json.loads(response)
+        for f in fetched:
+            phone_number = f.get("phone_number")
+            match = re.match("\(?([0-9]{3})\)?[-]?([0-9]{3})[-]?([0-9]{4})$",phone_number)
+            assert match is not None
+            self.assertEquals(len(match.groups()),3)
+
+    # check that number of comments is a number
+    def test_number_of_comments(self):
+        response = app.get_results(1)
+        fetched = json.loads(response)
+        report_count = fetched[0].get("report_count")
+        match = re.match("([\d])+$",report_count)
+        assert match is not None
+
+    # check that all numbers of comments are numbers
+    def test_all_number_of_comments(self):
+        response = app.get_results()
+        fetched = json.loads(response)
+        for f in fetched:
+            report_count = f.get("report_count")
+            match = re.match("([\d])+$",report_count)
+            assert match is not None
+            print report_count
+
+    
 
 
 if __name__ == "__main__":
